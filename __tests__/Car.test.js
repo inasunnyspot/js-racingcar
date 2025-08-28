ecc4cfabc2603f1ad124adfe3b86b2853a948784
Jiahn <jayang@in4ucloud.com>
2025-08-29 00:03:20 +0900

    feat: setp2 단위 테스트 연습 요구 사항 구현

---
+++ __tests__/Car.test.js
@@ -0,0 +1,22 @@
+import Car from '../src/main';
+
+describe('Cart Raging Game', () => {
+  let car;
+
+  beforeEach(() => {
+    car = new Car('Max');
+  });
+
+  test('자동차는 이름을 상태로 가질 수 있다.', () => {
+    expect(car.name).toBe('Max');
+  });
+
+  test('자동차는 위치 값을 가지며, 초기 상태는 0이다.', () => {
+    expect(car.position).toBe(0);
+  });
+
+  test('자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.', () => {
+    car.moveForward();
+    expect(car.position).toBe(1);
+  });
+});
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-class Car {
+export default class Car {
   position = 0;
   name;
 

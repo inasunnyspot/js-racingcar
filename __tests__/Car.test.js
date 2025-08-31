0b63ef7ddd27e90a0dd7f1850125bd04cbcf2a96
Jiahn <jayang@in4ucloud.com>
2025-08-31 14:09:50 +0900

    refactor: postion에 직접 값을 할당 했을 경우데 대한 테스트 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,4 +1,4 @@
-import Car from '../src/main';
+import Car, { POSITION_ERR_MSG } from '../src/main';
 
 describe('Cart Raging Game', () => {
   let car;
@@ -13,6 +13,12 @@ describe('Cart Raging Game', () => {
 
   test('자동차는 위치 값을 가지며, 초기 상태는 0이다.', () => {
     expect(car.position).toBe(0);
+  });
+
+  test('자동차에 직접 위치를 할당 할 수 없다.', () => {
+    expect(() => {
+      car.position = -100;
+    }).toThrow(POSITION_ERR_MSG);
   });
 
   test('자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.', () => {
